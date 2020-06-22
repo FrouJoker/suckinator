895e8805fd0b93b1b31f42126659be73741e4fe9
FrouJoker <67285797+FrouJoker@users.noreply.github.com>
2020-06-23 03:44:40 +1000

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 clear
-echo "------------------"
-echo "|   Представься  |"
-echo "|----------------|"
-echo "| 1. Termux      |"
-echo "| 2. Другой Unix |"
-echo "|                |"
-echo "| Введите 1/2:   |"
+echo "--------------------"
+echo "|  Представься     |"
+echo "|------------------|"
+echo "| 1. Termux        |"
+echo "| 2. Другой Unix   |"
+echo "| 3. iSH           |"
+echo "|                  |"
+echo "| Введите 1/2/3:   |"
 echo "--------------------"
 read numb
 if [ $numb = "1" ]
@@ -29,9 +30,27 @@ else
 		else
 			apt install python3 python3-pip dos2unix
 			pip3 install requests colorama proxyscrape
-			cp ~/suckinator/suckinator.py /usr/bin/spymer
+			cp ~/suckinator/suckinator.py /usr/bin/suckinator.py
 			dos2unix /usr/bin/suckinator
 			chmod 777 /usr/bin/suckinator
 			chmod -R 777 ~/suckinator
 			suckinator
+		fi
 	else
+		if [ $numb = "3" ] 
+		then
+			apk add python
+			apk add python3
+			apk add dos2unix
+			pip3 install requests
+			pip3 install colorama
+			pip3 install proxyscrape
+			cp ~/spymer/spammer.py /usr/bin/spymer
+			dos2unix /usr/bin/spymer
+			chmod 777 /usr/bin/spymer
+			spymer
+		else
+			echo "Некорректный ввод"
+		fi
+	fi
+fi
